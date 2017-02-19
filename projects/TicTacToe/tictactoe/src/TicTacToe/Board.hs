dc07bad7d3f4627f6ede159b3b2c2035e1dec263
Nero Zuo <502350123@qq.com>
2017-02-19 22:00:37 +0800

    Doing TicTacToe

--- projects/TicTacToe/tictactoe/src/TicTacToe/Board.hs
+++ projects/TicTacToe/tictactoe/src/TicTacToe/Board.hs
@@ -61,3 +61,42 @@ move p pos = do
     return (Just (p, pos))
 
 
+emptyBoard :: Board
+emptyBoard = ([], replicate 3 $ replicate 3 Nothing)
+
+
+chessBoard :: State Board a -> [[Maybe Player]]
+chessBoard bs = let (_, (_, board)) =  runState (bs >>= const get) emptyBoard
+                in board
+
+playerAt :: Position -> State Board a -> Maybe Player
+playerAt pos bs = board !! x !! y
+    where (x,y) = fromPos pos
+          board = chessBoard bs
+
+
+compress  :: Eq a => [a] -> Maybe a
+compress [] = Nothing
+compress (x:xs) = if all (==x) xs then Just x else Nothing
+
+findFirst :: (a -> Bool) -> [a] -> Maybe a
+findFirst _ [] = Nothing
+findFirst f (x:xs) = if f x then Just x else findFirst f xs
+
+whoWon :: State Board a -> Maybe Player
+whoWon bs = join $ findFirst (/= Nothing) [fRow, sRow, tRow, fCol, sCol, tCol, xx, yy]
+    where board = chessBoard bs
+          fRow = sequenceA (board !! 0) >>= compress
+          sRow = sequenceA (board !! 1) >>= compress
+          tRow = sequenceA (board !! 2) >>= compress
+          fCol = sequenceA (map (!! 0) board) >>= compress
+          sCol = sequenceA (map (!! 1) board) >>= compress
+          tCol = sequenceA (map (!! 2) board) >>= compress
+          xx   = sequenceA (zipWith (!!) board [0,1,2]) >>= compress
+          yy   = sequenceA (zipWith (!!) board [2,1,0]) >>= compress
+
+
+
+isDraw :: State Board a -> Bool
+isDraw bs = isJust $ sequenceA $ map sequenceA board
+    where board = chessBoard bs
